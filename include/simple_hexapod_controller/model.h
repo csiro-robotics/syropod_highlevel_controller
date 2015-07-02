5b409fdb6a9e345b879fe555ad4c6358472fbc2e
sa-asl-lab <Thomas.Lowe@csiro.au>
2015-07-02 14:37:36 +1000

    updated, better system

--- include/simple_hexapod_controller/model.h
+++ include/simple_hexapod_controller/model.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "standardIncludes.h"
 #include "quat.h"
+#include <../../../bullet3/btgui/lua-5.2.3/src/llimits.h>
 #include <Eigen/src/Core/Matrix.h>
 #include <boost/concept_check.hpp>
+#define timeDelta (1.0/50.0)
 
 // hard coded to 3 joints
 struct Leg
@@ -17,11 +19,11 @@ struct Leg
   Vector3d kneeOffset;
   Vector3d tipOffset;
   
-  bool onGround;
+//  bool onGround;
   Vector3d localTipPosition; // relative to root
 
   // sets angles to reach local position relative to root
-  void applyLocalIK(const Vector3d &tipTarget)
+  void applyLocalIK(const Vector3d &tipTarget, bool updateTipPos = true)
   {
     // application of cosine rule
     Vector3d target = tipTarget - rootOffset; // since rootOffset is fixed in root's space
@@ -38,7 +40,8 @@ struct Leg
     liftAngle = targetAngleOffset + lift;
     double kneeBend = acos((sqr(femurLength)+sqr(tibiaLength)-sqr(targetLength))/(2.0*femurLength*tibiaLength));
     kneeAngle = tibiaAngleOffset + kneeBend;
-    applyFK();
+    if (updateTipPos)
+      applyFK();
   }
   void applyWorldIK(const Pose &rootPose, const Vector3d &worldTipTarget)
   {
@@ -80,7 +83,6 @@ struct Leg
 struct Model
 {
   Leg legs[3][2]; // front to back, left to right
-  Pose rootPose;
   Model()
   {
     for (int l = 0; l<3; l++)
@@ -92,12 +94,13 @@ struct Model
         leg.hipOffset  = Vector3d(0.05, 0, 0);
         leg.kneeOffset = Vector3d(0.5, 0, 0);
         leg.tipOffset  = Vector3d(1.0, 0, 0);
-        leg.onGround = true;
+   //     leg.onGround = true;
         leg.init(0,0,0);
       }
     }
   }
-  
+/*  
+  Pose rootPose;
   // move leg tip (normally for swing leg)
   void moveLeg(int leg, int side, const Vector3d &worldTipTarget)
   {
@@ -117,47 +120,144 @@ struct Model
     }
     rootPose = targetPose; 
   }
+  */
 };
 
 struct TripodWalk
 {
   Model *model;
   Pose targetPose;
-  double clearance;
+  double stepFrequency;
+  double bodyClearance;
+  double stepClearance;
   Vector3d stanceLegYaws;
   Vector3d footSpreadDistances;
   double minFootprintRadius;
   double stanceRadius; // affects turning circle
   Vector3d localStanceTipPositions[3][2];
+  Vector2d localVelocity;
+  double curvature;
+  struct LegStepper
+  {
+    double phase; // 0 to 2pi
+    Vector2d velocity; // length gives ground velocity
+    Vector2d strideVector; // length gives stride length
+    
+    Vector3d getPosition(double liftHeight)
+    {
+      Vector3d strideVec(strideVector[0], strideVector[1], 0);
+      if (abs(phase) < pi/2.0) // stance
+      {
+        // I use a quartic to let the trajectory decelerate, but much flatter stance height than using a quadratic
+        double t = phase*2.0/pi;
+        Vector3d pos = strideVec * 0.5 * t;
+        pos[2] = liftHeight*0.5 * (pow(t, 4.0) - 1.0);
+      }
+      else // swing
+      {
+        Vector3d nodes[4];
+        nodes[0] = -strideVec*0.5;
+        nodes[3] = strideVec*0.5;
+        Vector3d lift(0,0,liftHeight*2.0);
+        nodes[1] = nodes[0] + (lift - strideVec)/3.0;
+        nodes[2] = nodes[0] + (lift + strideVec)/3.0;
+        return cubicBezier(nodes, (phase - pi/2.0)/pi);
+      }
+    }
+  } legSteppers[3][2];
   
   // Determines the basic stance pose which the hexapod will try to maintain, by finding the largest footprint radius that each leg can achieve for the specified level of clearance
-  // clearance- 0 to 1, 1 is vertical legs
+  // stepFrequency- preferred step cycles per second
+  // bodyClearance, stepClearance- 0 to 1, 1 is vertical legs
   // stanceLegYaws- natural yaw pose per leg
   // minYawLimits- the minimum yaw (or hip) joint limit around centre for each leg
-  TripodWalk(Model *model, double clearance, const Vector3d &stanceLegYaws, const Vector3d &minYawLimits) : model(model), clearance(clearance), stanceLegYaws(stanceLegYaws)
+  TripodWalk(Model *model, double stepFrequency, double bodyClearance, double stepClearance, const Vector3d &stanceLegYaws, const Vector3d &minYawLimits) : model(model), stepFrequency(stepFrequency), bodyClearance(bodyClearance), stepClearance(stepClearance), stanceLegYaws(stanceLegYaws)
   {
-    ASSERT(clearance >= 0 && clearance < 1.0);
+    ASSERT(bodyClearance >= 0 && bodyClearance < 1.0);
+    ASSERT(stepClearance >= 0 && stepClearance < 1.0);
     minFootprintRadius = 1e10;
     for (int l = 0; l<3; l++)
     {
-      double horizontalRange = sqrt(sqr(model->legs[l].legLength) - sqr(clearance*model->legs[l].legLength));
+      double horizontalRange = sqrt(sqr(model->legs[l].legLength) - sqr(bodyClearance*model->legs[l].legLength));
       double theta = minYawLimits[l] - abs(stanceLegYaws[l]);
       double cotanTheta = tan(0.5*pi - theta);
       double rad = solveQuadratic(sqr(cotanTheta), -2.0*horizontalRange, sqr(horizontalRange));
       footSpreadDistances[l] = rad * cotanTheta;
-      minFootprintRadius = min(minFootprintRadius, rad);
+      double footprintDownscale = 0.8; // this is because the step cycle exceeds the ground footprint in order to maintain velocity
+      minFootprintRadius = min(minFootprintRadius, rad*footprintDownscale);
       for (int side = 0; side<2; side++)
       {
-        localStanceTipPositions[l][s] = model->legs[l].rootOffset + footSpreadDistances[l]*Vector3d(cos(stanceLegYaws[l]), sin(stanceLegYaws[l]), -clearance*model->legs[l].legLength);
+        localStanceTipPositions[l][s] = model->legs[l].rootOffset + footSpreadDistances[l]*Vector3d(cos(stanceLegYaws[l]), sin(stanceLegYaws[l]), -bodyClearance*model->legs[l].legLength);
       }
     }
     stanceRadius = abs(localStanceTipPositions[1][0][1]);
+    int i = 0;
+    for (int l = 0; l<3; l++)
+    {
+      for (int s = 0; s<2; s++)
+      {
+        legSteppers[l][s].phase = pi * double((i++)%2); 
+        legSteppers[l][s].velocity = Vector3d(0,0,0);
+      }
+    }
+    localVelocity = Vector2d(0,0);
+    curvature = 0;
   }
 
+  
   // curvature is 0 to 1 so 1 is rotate on the spot, 0.5 rotates around leg stance pos
-  void walk(const Vector3d &localDirection, double curvature)
+  // bodyOffset is body pose relative to the basic stance pose, note that large offsets may prevent achievable leg positions
+  void walk(Vector2d newLocalVelocity, double newCurvature, const Pose *bodyOffset = NULL)
   {
+    // this block assures the local velocity and curvature values don't change too quickly
+    double maxAcceleration = 3.0;
+    double maxCurvatureSpeed = 1.0;
+    Vector3d diff = localVelocity - newLocalVelocity;
+    double diffLength = diff.norm();
+    localVelocity = newLocalVelocity + diff * max(0, (diffLength-maxAcceleration*timeDelta)/diffLength);
+    double dif = curvature - newCurvature;
+    curvature = newCurvature + dif * max(0, (abs(dif)-maxCurvatureSpeed*timeDelta)/abs(dif));
+    
+    double speed = localVelocity.norm();
+    
+    // max out the walk speed to what is achievable with stepFrequency. Later we could increase step frequency instead
+    double stride = speed / (2.0*stepFrequency);
+    if (stride > minFootprintRadius)
+    {
+      localVelocity *= minFootprintRadius / stride;
+      speed *= minFootprintRadius / stride;
+    }
+    
+    double turningRadius = getTurningRadius(curvature);
+    Vector2d turningPoint = turningRadius * Vector2d(-localVelocity[1], localVelocity[0])/speed; // the point it is turning around
+    double angularVelocity = speed/(turningRadius + stanceRadius); // we make the speed argument refer to the outer leg, so turning on the spot still has a meaningful speed argument
+    
+    for (int l = 0; l<3; l++)
+    {
+      for (int s = 0; s<2; s++)
+      {
+        LegStepper &legStepper = legSteppers[l][s];
+        Leg &leg = model->legs[s][l];
+        Vector2d toTip = turningPoint + Vector2d(leg.localTipPosition[0], leg.localTipPosition[1]);
+        legStepper.velocity = angularVelocity * Vector2d(-toTip[1], toTip[0]) / (2.0*stepFrequency);
+        
+        legStepper.phase += 2.0*pi*stepFrequency*timeDelta;
+        if (legStepper.phase > 2.0*pi)
+          legStepper.phase -= 2.0*pi;
+        leg.applyLocalIK(localStanceTipPositions[l][s] + legStepper.getPosition(stepClearance*leg.legLength));
+      }
+    }
+    
+    if (bodyOffset != NULL)
+    {
+      for (int l = 0; l<3; l++)
+        for (int side = 0; side<2; side++)
+          model->legs[l][side].applyLocalIK(bodyOffset->inverseTransformVector(leg.localTipPosition), false); // false means we don't update local tip position, as it is needed above in step calculations
+    }
   }
 protected:
-  
+  double getTurningRadius(double curvature)
+  {
+    return (stanceRadius / max(0.0001, curvature)) - stanceRadius;
+  }
 };
