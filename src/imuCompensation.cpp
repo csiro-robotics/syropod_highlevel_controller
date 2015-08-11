bb55940741a0fc4b6812aa441be74cfac92b01b7
sa-asl-lab <Thomas.Lowe@csiro.au>
2015-08-11 16:53:25 +1000

    oops, bad merge before

--- src/imuCompensation.cpp
+++ src/imuCompensation.cpp
@@ -157,15 +157,9 @@ void calculatePassiveAngularFrequency()
   double inputAngle = 3.0*timex;
   double decayRate = 0.25;
   // lossy integrator
-<<<<<<< HEAD
-  vel = (vel + acc*timeDelta) / (1.0 + decayRate*timeDelta);
-  states[stateIndex] = Vector2d(vel, acc);
-=======
-
   vel = (vel + (acc - mean[1])*timeDelta) / (1.0 + decayRate*timeDelta);
   states[stateIndex] = Vector2d(vel - mean[0], acc - mean[1]);
   stateIndex = (stateIndex + 1) % numStates; // poor man's circular queue!
->>>>>>> 8c0335b549ec1bc1b0e20d1861eff7bd178bcdf3
   debugDraw->plot(states); // should look noisy and elliptical
 
   
@@ -191,18 +185,7 @@ void calculatePassiveAngularFrequency()
   double x = normalisedStates[stateIndex][0] * -sin(theta) + normalisedStates[stateIndex][1] * cos(theta);
   relativeStates[stateIndex] = Vector2d(x,y);
   for (int i = 0; i<numStates; i++)
-<<<<<<< HEAD
     sumUnrotated += relativeStates[i];
-=======
-  {
-    double theta = omega * (timex + (double)(-numStates + (i+numStates-stateIndex)%numStates)*timeDelta);
-    double x = normalisedStates[i][0] * sin(theta) + normalisedStates[i][1] * cos(theta);
-    double y = normalisedStates[i][0] * cos(theta) + normalisedStates[i][1] * -sin(theta);
-    unrotatedStates[i] = Vector2d(x,y);
-    sumUnrotated += unrotatedStates[i];
-  }
-  unrotatedStates.push_back(Vector2d(0,0));
->>>>>>> 8c0335b549ec1bc1b0e20d1861eff7bd178bcdf3
   double phaseOffset = atan2(sumUnrotated[0], sumUnrotated[1]);
   cout << "rolling phase offset estimate: " << phaseOffset << endl;
   debugDraw->plot(relativeStates); // this should cluster around a particular phase
