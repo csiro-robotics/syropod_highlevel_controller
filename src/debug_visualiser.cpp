dc1f224515be166e41a3e020fdeb6e76f09b20c2
Jayasinghe, Oshada (Data61, Pullenvale) <Oshada.Jayasinghe@data61.csiro.au>
2019-10-23 16:31:12 +1000

    Modified copyrights notice as per OHM in Github

--- src/debug_visualiser.cpp
+++ src/debug_visualiser.cpp
@@ -1,13 +1,10 @@
-//*********************************************************************************************************************
-// * CSIRO Autonomous Systems Laboratory
-// * Queensland Centre for Advanced Technologies
-// * PO Box 883, Kenmore, QLD 4069, Australia
-// *
-// * (c) Copyright CSIRO 2017
-// *
-// * All rights reserved, no part of this program may be used
-// * without explicit permission of CSIRO
-//*********************************************************************************************************************
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2019
+// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+// ABN 41 687 119 230
+//
+// Author: Fletcher Talbot
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "syropod_highlevel_controller/debug_visualiser.h"
 #include "syropod_highlevel_controller/walk_controller.h"
